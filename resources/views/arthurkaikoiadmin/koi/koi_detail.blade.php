9e26dd238ec5842d6ba3a67903caf9b70b5cc6a8
Cynthia <cynthiaamanurung@gmail.com>
2024-07-26 17:49:58 +0700

    modified:   resources/views/arthurkaikoiadmin/koi/koi_detail.blade.php

--- resources/views/arthurkaikoiadmin/koi/koi_detail.blade.php
+++ resources/views/arthurkaikoiadmin/koi/koi_detail.blade.php
@@ -161,7 +161,7 @@
         .swiper-slide img {
             display: block;
             width: 100%;
-            height: 100%;
+            height: auto;
             object-fit: cover;
         }
 
